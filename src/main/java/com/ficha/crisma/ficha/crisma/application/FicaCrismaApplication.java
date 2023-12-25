190dd3ee6bcd8655c08ea7c6acb79623f60fbd00
Marcelo Magrinelli <marcelovbm@gmail.com>
2023-12-25 07:25:45 -0300

    Modificando pacote para classe starter

--- src/main/java/com/ficha/crisma/ficha/crisma/application/FicaCrismaApplication.java
+++ src/main/java/com/ficha/crisma/ficha/crisma/application/FicaCrismaApplication.java
@@ -3,7 +3,7 @@ package com.ficha.crisma.ficha.crisma.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.ficha.crisma.ficha.crisma"})
 public class FicaCrismaApplication {
 
     public static void main(String[] args) {
